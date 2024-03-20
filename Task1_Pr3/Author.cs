0af7caaa39d50d6784d04c822db027e3ee477447
Dimab01112 <118934619+Dimab01112@users.noreply.github.com>
2024-03-20 15:35:45 +0200

    Update Author.cs

--- Task1_Pr3/Author.cs
+++ Task1_Pr3/Author.cs
@@ -1,24 +1,24 @@
 using System;
 
-class Author
+public class Author
 {
     private string authorName;
-
-    public Author(string authorName)
-    {
-        this.authorName = authorName;
-    }
-
-    public void Show()
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Author: " + authorName);
-        Console.ResetColor();
-    }
 
     public string AuthorName
     {
         get { return authorName; }
         set { authorName = value; }
+    }
+
+    public Author(string name)
+    {
+        authorName = name;
+    }
+
+    public void Show()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Author: {authorName}");
+        Console.ResetColor();
     }
 }
