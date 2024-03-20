c7bc2f30073066e7643a87014f6c5a3b3254989c
Dimab01112 <118934619+Dimab01112@users.noreply.github.com>
2024-03-20 15:46:50 +0200

    Update Figure.cs

--- Task2_Pr3/Figure.cs
+++ Task2_Pr3/Figure.cs
@@ -19,34 +19,20 @@ public class Figure
         return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
     }
 
-    public void PerimeterCalculator()
+  public void PerimeterCalculator()
+{
+    double perimeter = 0;
+    string figureName = string.Join("", points.Select(point => point.Label)); 
+
+    for (int i = 0; i < points.Length; i++)
     {
-        double perimeter = 0;
-        for (int i = 0; i < points.Length; i++)
-        {
-            Point currentPoint = points[i];
-            Point nextPoint = points[(i + 1) % points.Length];
-            perimeter += LengthSide(currentPoint, nextPoint);
-        }
-
-        string figureName;
-        switch (points.Length)
-        {
-            case 3:
-                figureName = "Triangle";
-                break;
-            case 4:
-                figureName = "Square";
-                break;
-            case 5:
-                figureName = "Pentagon";
-                break;
-            default:
-                figureName = "Unknown Figure";
-                break;
-        }
-
-        Console.WriteLine($"Name of the figure: {figureName}");
-        Console.WriteLine($"Perimeter of the figure: {perimeter}");
+        Point currentPoint = points[i];
+        Point nextPoint = points[(i + 1) % points.Length];
+        perimeter += LengthSide(currentPoint, nextPoint);
     }
+
+    Console.WriteLine($"Name of the figure: {figureName}");
+    Console.WriteLine($"Perimeter of the figure: {perimeter}");
+}
+
 }
